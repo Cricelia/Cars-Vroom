2a1aae7ea652230349dbfd0a37c2bc561051b020
Cricelia <cpradoperez@outlook.com>
2025-05-09 15:00:05 -0700

    Phase 1
    code the Vehicle class and Dealership class but didn't add logic into the methods of Dealership class

--- Cars Vroom/src/main/java/com/CarsVroom/Main.java
+++ Cars Vroom/src/main/java/com/CarsVroom/Main.java
@@ -1,7 +1,17 @@
 package com.CarsVroom;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+
+        File file = new File("src/main/java/com/CarsVroom/data/inventory.csv");
+
+        if (file.exists()) {
+            System.out.println("CSV file found: " + file.getAbsolutePath());
+        } else {
+            System.out.println("CSV file not found!");
+        }
 
     }
 }
--- Cars Vroom/src/main/java/com/CarsVroom/model/Dealership.java
+++ Cars Vroom/src/main/java/com/CarsVroom/model/Dealership.java
@@ -1,4 +1,47 @@
 package com.CarsVroom.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
-}
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+    
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+     public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+     }
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+    public List<Vehicle> getAllVehicles() {
+        return new ArrayList<>(inventory);
+    }
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+    }
