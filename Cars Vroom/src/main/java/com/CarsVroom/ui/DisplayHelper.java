bea680009328a41d376881d62782d7b587065107
Cricelia <cpradoperez@outlook.com>
2025-05-16 10:07:31 -0700

    Phase 3

---
+++ Cars Vroom/src/main/java/com/CarsVroom/ui/DisplayHelper.java
@@ -0,0 +1,53 @@
+package com.CarsVroom.ui;
+
+
+import com.CarsVroom.model.Dealership;
+
+public class DisplayHelper {
+    private Dealership dealership;
+
+    public DisplayHelper(Dealership dealership) {
+        this.dealership = dealership;
+    }
+
+    public void processGetAllVehicles() {
+        // To be implemented
+    }
+
+    public void processGetByPrice() {
+        // To be implemented
+    }
+
+    public void processGetByMakeModel() {
+        // To be implemented
+    }
+
+    public void processGetByYear() {
+        // To be implemented
+    }
+
+    public void processGetByColor() {
+        // To be implemented
+    }
+
+    public void processGetByMileage() {
+        // To be implemented
+    }
+
+    public void processGetByType() {
+        // To be implemented
+    }
+
+    public void processAddVehicle() {
+        // To be implemented
+    }
+
+    public void processRemoveVehicle() {
+        // To be implemented
+    }
+
+    public void processQuit() {
+        // To be implemented
+    }
+}
+
--- Cars Vroom/src/main/java/com/CarsVroom/ui/UserInterface.java
+++ Cars Vroom/src/main/java/com/CarsVroom/ui/UserInterface.java
@@ -5,25 +5,46 @@ import com.CarsVroom.model.Vehicle;
 
 import java.util.ArrayList;
 
+
 public class UserInterface {
     private Dealership dealership;
+    private DisplayHelper displayHelper;
 
     public void display() {
         init();
+
         // menu loop, scanner input, switch/case goes here eventually
-    }
 
+        DisplayHelper helper = new DisplayHelper(dealership);
+        int choice = 0;
+        switch (choice) {
+            case 1 -> helper.processGetAllVehicles();
+            case 2 -> helper.processGetByPrice();
+            case 3 -> helper.processGetByMakeModel();
+            case 4 -> helper.processGetByYear();
+            case 5 -> helper.processGetByColor();
+            case 6 -> helper.processGetByMileage();
+            case 7 -> helper.processGetByType();
+            case 8 -> helper.processAddVehicle();
+            case 9 -> helper.processRemoveVehicle();
+            case 10 -> helper.processQuit();
+
+
+        }
+
+    }
     private void init() {
-        DealershipFileManager fileManager = new DealershipFileManager();
-        this.dealership = fileManager.getDealership();
-    }
+            DealershipFileManager fileManager = new DealershipFileManager();
+            this.dealership = fileManager.getDealership();
+            this.displayHelper = new DisplayHelper(dealership);
+        }
 
-    private void displayVehicles(ArrayList<Vehicle> vehicles) {
-        // print each vehicle
-    }
+        private void displayVehicles (ArrayList < Vehicle > vehicles) {
+            // print each vehicle
+        }
 
-    public void processAllVehiclesRequest() {
-        ArrayList<Vehicle> vehicles = dealership.getAllVehicles();
-        displayVehicles(vehicles);
+        public void processAllVehiclesRequest () {
+            ArrayList<Vehicle> vehicles = dealership.getAllVehicles();
+            displayVehicles(vehicles);
+        }
     }
-}
